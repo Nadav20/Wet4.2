f853928bf664ed170222395cbfdccb020ced8324
Nadav20 <128716311+Nadav20@users.noreply.github.com>
2024-08-17 17:25:16 +0300

    Update malloc_2.cpp
    works

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -1,490 +1,146 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
 #include <unistd.h>
-#include <math.h>
-#include <string.h>
-class Block_Node{
-private:
-    Block_Node* prev;
+#include <cstring>
+#include <sys/mman.h>
+#include <iostream>
+
+struct Block_Node {
+    size_t size;
+    bool is_free;
     Block_Node* next;
-    //int num_of_elements_in_block;
-    int* first_allocated_byte_in_block;
-    size_t real_size_of_block;
-    bool block_is_free_flag;
-    int* buddy_address;
-public:
-    Block_Node(int* first_allocated_byte_in_block, size_t size_of_block);
-    Block_Node* getPrev();
-    Block_Node* getNext();
-    int* getFirstAllocatedByteInBlock();
-    size_t getSizeOfBlock();
-    int* getBuddyAddress();
-    //int getNumOfElementsInBlock();
-    bool getBlockIsFreeFlag();
-    void setPrev(Block_Node* prev);
-    void setNext(Block_Node* next);
-    void setFirstAllocatedByteInBlock(int* byte);
-    void setSizeOfBlock(size_t size);
-    void setBlockIsFreeFlag(bool flag);
-    void setBuddyAddress(int* buddy_address);
-    //void setNumOfElementsInBlock(int num_of_elements_in_block);
+    Block_Node* prev;
 };
-class List{
-private:
-    Block_Node* head;
-    Block_Node* tail;
-    // TODO - add list counter of?
-public:
-    List();
-    List(Block_Node* head);
-    ~List();
-    Block_Node* getHead();
-    Block_Node* getTail();
-    void setHead(Block_Node* head);
-    void setTail(Block_Node* tail);
-    void insertToTail(Block_Node* new_tail);
-    Block_Node* removeFromList(int* first_allocated_byte_in_block);
-    Block_Node* removeHead();
-    void insert_by_first_byte(Block_Node* node);
 
-};Block_Node::Block_Node(int* first_allocated_byte_in_block, size_t size_of_block)
-{
-    this->next = nullptr;
-    this->prev = nullptr;
-    //this->num_of_elements_in_block = 0;
-    this->block_is_free_flag = false;
-    this->first_allocated_byte_in_block = first_allocated_byte_in_block + sizeof(Block_Node);
-    this->real_size_of_block = size_of_block + sizeof(Block_Node);
-    this->buddy_address = nullptr;
-}
-int* Block_Node::getFirstAllocatedByteInBlock()
-{
-    return this->first_allocated_byte_in_block;
-}
-size_t Block_Node::getSizeOfBlock()
-{
-    return this->real_size_of_block - sizeof(Block_Node);
-}
-bool Block_Node::getBlockIsFreeFlag()
-{
-    return this->block_is_free_flag;
-}
-Block_Node* Block_Node::getPrev()
-{
-    return this->prev;
-}
-Block_Node* Block_Node::getNext()
-{
-    return this->next;
-}
-/*int Block_Node::getNumOfElementsInBlock()
-{
-    return this->num_of_elements_in_block;
-}*/
-int* Block_Node::getBuddyAddress()
-{
-    return this->buddy_address;
-}
-void Block_Node::setPrev(Block_Node* prev)
-{
-    this->prev = prev;
-    prev->next = this;
-}
-void Block_Node::setNext(Block_Node* next)
-{
-    this->next = next;
-    next->prev = this;
-}
-void Block_Node::setFirstAllocatedByteInBlock(int* byte)
-{
-    this->first_allocated_byte_in_block = byte + sizeof(Block_Node);
-}
-void Block_Node::setSizeOfBlock(size_t size)
-{
-    this->real_size_of_block = size + sizeof(Block_Node);
-}
-void Block_Node::setBlockIsFreeFlag(bool flag)
-{
-    this->block_is_free_flag = flag;
-}
-/*void Block_Node::setNumOfElementsInBlock(int num_of_elements_in_block)
-{
-    this->num_of_elements_in_block = num_of_elements_in_block;
-}*/
+Block_Node* list_head = nullptr;
+size_t counter_free_blocks = 0;
+size_t counter_free_bytes = 0;
+size_t counter_allocated_blocks = 0;
+size_t counter_allocated_bytes = 0;
+size_t counter_meta_data_bytes = 0;
 
-void Block_Node::setBuddyAddress(int* buddy_address)
-{
-    this->buddy_address = buddy_address;
-}
-List::List()
-{
-    this->head = nullptr;
-    this->tail = nullptr;
-}
-List::List(Block_Node* head)
-{
-    this->head = head;
-    this->tail = this->head;
-}
-List::~List()
-{
-    Block_Node* current = this->getHead();
-    Block_Node* next = nullptr;
-    while (current != nullptr)
-    {
-        next = current->getNext();
-        delete current->getFirstAllocatedByteInBlock();
-        delete current;
-        current = next;
+
+void* smalloc(size_t size){
+    if(size == 0 || size > 1e8){
+        return NULL;
     }
-}
-Block_Node* List::getHead()
-{
-    return this->head;
-}
-Block_Node* List::getTail()
-{
-    return this->tail;
-}
-void List::setHead(Block_Node* head)
-{
-    this->head = head;
-}
-void List::setTail(Block_Node* tail)
-{
-    this->tail = tail;
-}
-void List::insertToTail(Block_Node* new_tail)
-{
-    if (this->head == nullptr) // the list is empty
-    {
-        this->setHead(new_tail);
-        this->tail = this->head;
-    }
-    else
-    {
-        this->tail->setNext(new_tail);
-        this->setTail(new_tail);
-    }
-}
-Block_Node* List::removeFromList(int* first_allocated_byte_in_block)
-{
-    Block_Node* current = this->head;
-    if (current->getFirstAllocatedByteInBlock() == first_allocated_byte_in_block) // we want to remove the head of the list
-    {
-        return removeHead();
-    }
-    while (current != nullptr)
-    {
-        if (current->getFirstAllocatedByteInBlock() == first_allocated_byte_in_block)
-        {
-            current->getPrev()->setNext(current->getNext());
-            if (current->getNext() != nullptr)
-            {
-                current->getNext()->setPrev(current->getPrev());
-            }
-            else
-            {
-                this->setTail(current->getPrev());
-            }
-            current->setPrev(nullptr); // not sure if needed
-            current->setNext(nullptr); // not sure if needed
-            return current;
+    Block_Node* current = list_head;
+    while(current != NULL){
+        if(current->is_free && current->size >= size){
+            current->is_free = false;
+            counter_free_blocks--;
+            counter_free_bytes -= current->size;
+            return (char*)(current) + sizeof(Block_Node);
         }
-        current = current->getNext();
+        current = current->next;
     }
-    return nullptr;
-}
-Block_Node* List::removeHead()
-{
-    if (this->head == nullptr)
-    {
-        return nullptr;
+    void* request = sbrk(size + sizeof(Block_Node));
+    if(request == (void*)(-1)){//Nizan- moved above
+        return NULL;
     }
-    Block_Node* old_head = new Block_Node(this->head->getFirstAllocatedByteInBlock(),this->head->getSizeOfBlock());
-    head = head->getNext();
-    delete head->getPrev();
-    head->setPrev(nullptr);
-    return old_head;
-}
+    counter_allocated_blocks++;
+    counter_allocated_bytes += size;
+    counter_meta_data_bytes += sizeof(Block_Node);
+    Block_Node* new_block = (Block_Node*)request;
+    new_block->size = size;
+    new_block->is_free = false;
+    new_block->next = NULL;
+    new_block->prev = NULL;
+    //TESTS: wrote this again, old one not good, switch
+    current = list_head;
+    if (list_head == nullptr) {
+        list_head = new_block;
+    }
 
-void List::insert_by_first_byte(Block_Node* node)
-{
-    if (this->head == nullptr)
-    {
-        this->setHead(node);
-        this->setTail(node);
-        return;
+        // If the new node should be inserted at the beginning
+    else if (new_block < list_head) {
+        new_block->next = list_head;
+        list_head->prev = new_block;
+        list_head = new_block;
     }
-    Block_Node* current_node = this->head;
-    while (current_node != nullptr)
-    {
-        if (node->getFirstAllocatedByteInBlock() < current_node->getFirstAllocatedByteInBlock())
-        {
-            if (current_node->getFirstAllocatedByteInBlock() == this->head->getFirstAllocatedByteInBlock()) // current_node is head
-            {
-                this->setHead(node);
-                current_node->setPrev(node);
-                node->setNext(current_node);
-            }
-            else
-            {
-                current_node->getPrev()->setNext(node);
-                node->setPrev(current_node->getPrev());
-                node->setNext(current_node);
-                current_node->setPrev(node);
-            }
-            return;
+
+    else {
+        // Find the correct position to insert the new node
+        while (current->next != nullptr && current->next < new_block) {
+            current = current->next;
         }
-        current_node = current_node->getNext();
-    }
-    this->insertToTail(node);
-}
 
-
-List* blocks_list;
-void* smalloc(size_t size)
-{
-    if (size == 0)
-    {
-        return NULL;
-    }
-    if (size > pow(10,8))
-    {
-        return NULL;
-    }
-    int* first_byte_in_the_allocated_block;
-    Block_Node* current_block_node = blocks_list->getHead();
-    while (current_block_node != nullptr)
-    {
-        if (current_block_node->getSizeOfBlock() >= size && current_block_node->getBlockIsFreeFlag() == true)
-        {
-            first_byte_in_the_allocated_block = current_block_node->getFirstAllocatedByteInBlock();
-            current_block_node->setBlockIsFreeFlag(false);
-            return first_byte_in_the_allocated_block;
+        // Insert the new node
+        new_block->next = current->next;
+        new_block->prev = current;
+        if (current->next != nullptr) {
+            current->next->prev = new_block;
         }
-        current_block_node = current_block_node->getNext();
+        current->next = new_block;
+
     }
-    int actual_size = size + sizeof(Block_Node);// maybe should be with "+ sizeof(Block_Node*)"?
-    first_byte_in_the_allocated_block = (int*)sbrk(actual_size);
-    if (*first_byte_in_the_allocated_block == -1)
-    {
-        return NULL;
-    }
-    Block_Node* new_block_node = new Block_Node(first_byte_in_the_allocated_block,size);
-    if (new_block_node == nullptr)
-    {
-        return NULL;
-    }
-    blocks_list->insertToTail(new_block_node);
-    return first_byte_in_the_allocated_block;
+
+    size_t s = sizeof(Block_Node);
+
+    return (char*)new_block + s; //TESTS : char* for bytes and than and num of bytes
 }
-void* scalloc(size_t num, size_t size)
-{
-    if (size == 0 || num == 0)
-    {
-        return NULL;
-    }
+
+void* scalloc(size_t num, size_t size){
     if (size * num > pow(10,8))
     {
         return NULL;
     }
-    int* first_byte_in_the_allocated_block;
-    Block_Node* current_block_node = blocks_list->getHead();
-    while (current_block_node != nullptr)
-    {
-        if (current_block_node->getSizeOfBlock() >= size * num && current_block_node->getBlockIsFreeFlag() == true)
-        {
-            first_byte_in_the_allocated_block = current_block_node->getFirstAllocatedByteInBlock();
-            current_block_node->setBlockIsFreeFlag(false);
-            memset(first_byte_in_the_allocated_block, 0, size*num);
-            return first_byte_in_the_allocated_block;
-        }
-        current_block_node = current_block_node->getNext();
-    }
-    int actual_size = (size * num) + sizeof(Block_Node);// maybe should be with "+ sizeof(Block_Node*)"?
-    first_byte_in_the_allocated_block = (int*)sbrk(actual_size);
-    if (*first_byte_in_the_allocated_block == -1)
-    {
+    void* request = smalloc(num * size);
+    if(request == NULL){
         return NULL;
     }
-    Block_Node* new_block_node = new Block_Node(first_byte_in_the_allocated_block, size * num);
-    if (new_block_node == nullptr)
-    {
-        return NULL;
-    }
-    if (blocks_list->getHead() == nullptr) // blocks_list is empty
-    {
-        
-        blocks_list->setHead(new_block_node);
-        blocks_list->setTail(new_block_node);
-    }
-    else
-    {
-        blocks_list->insertToTail(new_block_node);
-    }
-    memset(first_byte_in_the_allocated_block, 0, size*num);
-    return first_byte_in_the_allocated_block;
+    memset(request, 0, num * size);
+    return request;
 }
-void sfree(void* p)
-{
-    if (p == NULL)
-    {
+
+void sfree(void* p){
+    if(p == NULL){
         return;
     }
-    Block_Node* current_block_node = blocks_list->getHead();
-    while (current_block_node != nullptr)
-    {
-        if (current_block_node->getFirstAllocatedByteInBlock() == (int*)p)
-        {
-            if (current_block_node->getBlockIsFreeFlag() == false)
-            {
-                current_block_node->setBlockIsFreeFlag(true);
-            }
-            return;
-        }
-        current_block_node = current_block_node->getNext();
-    } 
-}
-void* srealloc(void* oldp, size_t size)
-{
-    int actual_size;
-    if (size == 0)
-    {
-        return NULL;
-    }
-    if (size >= pow(10, 8))
-    {
-        return NULL;
-    }
-    if (oldp == nullptr) // NULL instead of nullptr?
-    {
-        actual_size = size + sizeof(Block_Node);// maybe should be with "+ sizeof(Block_Node*)"?
-        int* first_byte_in_the_allocated_block = (int*)sbrk(actual_size);
-        if (*first_byte_in_the_allocated_block == -1)
-        {
-            return NULL;
-        }
-        Block_Node* new_block_node = new Block_Node(first_byte_in_the_allocated_block, size);
-        if (new_block_node == nullptr)
-        {
-            return NULL;
-        }
-        blocks_list->insertToTail(new_block_node);
-        return first_byte_in_the_allocated_block;
-    }
-    
-    Block_Node* current_block_node = blocks_list->getHead();
-    while (current_block_node != nullptr)
-    {
-        if (current_block_node->getFirstAllocatedByteInBlock() == (int*)oldp)
-        {
-            if (size <= current_block_node->getSizeOfBlock())
-            {
-                // reuse the same block, e.g. don't do anything
-                return current_block_node->getFirstAllocatedByteInBlock();
-            }
-        }
-        current_block_node = current_block_node->getNext();
-    }
-
-    current_block_node = blocks_list->getHead();
-    while (current_block_node != nullptr)
-    {
-        if (current_block_node->getSizeOfBlock() >= size) 
-        {
-            if (current_block_node->getBlockIsFreeFlag() == true)
-            {
-                current_block_node->setFirstAllocatedByteInBlock((int*)oldp);
-                current_block_node->setBlockIsFreeFlag(false);
-                free(oldp);
-                return current_block_node->getFirstAllocatedByteInBlock();
-            }
-        }
-        current_block_node = current_block_node->getNext();
-    }
-
-    actual_size = size + sizeof(Block_Node);// maybe should be with "+ sizeof(Block_Node*)"?
-    int* first_byte_in_the_allocated_block = (int*)sbrk(actual_size);
-    if (*first_byte_in_the_allocated_block == -1)
-    {
-        return NULL;
-    }
-    Block_Node* new_block_node = new Block_Node(first_byte_in_the_allocated_block,size);
-    if (new_block_node == nullptr)
-    {
-        return NULL;
-    }
-    new_block_node->setFirstAllocatedByteInBlock((int*)oldp);
-    blocks_list->insertToTail(new_block_node);
-    free(oldp);
-    return blocks_list->getTail()->getFirstAllocatedByteInBlock();
+    Block_Node* block = (Block_Node*)((char*)p - sizeof(Block_Node)); //TESTS : char* for bytes
+    block->is_free = true;
+    counter_free_blocks++;
+    counter_free_bytes += block->size;
 }
 
-///////////////////////////////////// NADAV 5-9 Funcs //////////////////////////////////////////////
-
+void* srealloc(void* oldp, size_t size){
+    if(size == 0 || size > pow(10,8)){
+        return NULL;
+    }
+    if(oldp == NULL){
+        return smalloc(size);
+    }
+    Block_Node* block = (Block_Node*)((char*)oldp - sizeof(Block_Node));
+    if(block->size >= size){
+        return oldp;
+    }
+    void* newp = smalloc(size ); //TEST: not need to allocate more
+    if(newp == NULL){
+        return NULL;
+    }
+    memmove(newp, oldp, block->size);
+    sfree(oldp);
+    //free balance stays
+//    return newp + sizeof(Block_Node);//nizan
+    return newp;
+}
 
 size_t _num_free_blocks(){
-    int counter_free_blocks = 0;
-    Block_Node* current_block_node = blocks_list->getHead();
-    while (current_block_node != nullptr)
-    {
-        if (current_block_node->getBlockIsFreeFlag() == true)
-        {
-            counter_free_blocks++;
-        }
-        current_block_node = current_block_node->getNext();
-    }
     return counter_free_blocks;
 }
-
-
 size_t _num_free_bytes(){
-    int counter_free_bytes = 0;
-    Block_Node* current_block_node = blocks_list->getHead();
-    while (current_block_node != nullptr)
-    {
-        if (current_block_node->getBlockIsFreeFlag())
-        {
-            counter_free_bytes += current_block_node->getSizeOfBlock();
-        }
-        current_block_node = current_block_node->getNext();
-    }
     return counter_free_bytes;
 }
-
 size_t _num_allocated_blocks(){
-    int _num_allocated_blocks = 0;
-    Block_Node* current_block_node = blocks_list->getHead();
-    while (current_block_node != nullptr)
-    {
-        if (current_block_node->getBlockIsFreeFlag())
-        {
-            _num_allocated_blocks++;
-        }
-        current_block_node = current_block_node->getNext();
-    }
-    return _num_allocated_blocks;
+    return counter_allocated_blocks;
 }
-
-
-
 size_t _num_allocated_bytes(){
-    int counter_allocated_bytes = 0;
-    Block_Node* current_block_node = blocks_list->getHead();
-    while (current_block_node != nullptr)
-    {
-        counter_allocated_bytes += current_block_node->getSizeOfBlock();
-        current_block_node = current_block_node->getNext();
-    }
     return counter_allocated_bytes;
 }
-
-////////// not sure about the last two functions
 size_t _num_meta_data_bytes(){
-    return _num_allocated_blocks() * sizeof(Block_Node);
+    return counter_meta_data_bytes;
 }
-
 size_t _size_meta_data(){
     return sizeof(Block_Node);
 }
